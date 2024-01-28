625dab13092bc3e11ee75260171c8ba9de9e7e9b
Shashank Sharma <shashank.hacky0@gmail.com>
2024-01-28 16:02:39 +0530

    removed unnecary links

--- js/app.js
+++ js/app.js
@@ -1,9 +1,3 @@
-// preloader
-var loader = document.getElementById("preloader");
-    window.addEventListener("load", function(){
-        loader.style.display = "none"
-    })
-
 const btn = document.getElementById('menu-btn');
 const overlay = document.getElementById('overlay');
 const menu = document.getElementById('mobile-menu');
